a85aba4ad45b886543a01b102cb50ef40b57f95f
vagasilac <vagasilac@gmail.com>
2023-05-29 10:05:43 +0300

    environment config models index

--- sentierri_react_backend/config/config.js
+++ sentierri_react_backend/config/config.js
@@ -8,27 +8,31 @@ module.exports = {
     "password": process.env.DB_DEV_PASSWORD,
     "database": process.env.DB_DEV_NAME,
     "host": process.env.DB_DEV_HOST,
-    "dialect": process.env.DB_DEV_DIALECT
+    "dialect": process.env.DB_DEV_DIALECT,
+    "use_env_variable": process.env.USE_ENV_VARIABLE
   },
   "test": {
     "username": process.env.DB_TEST_USER,
     "password": process.env.DB_TEST_PASSWORD,
     "database": process.env.DB_TEST_NAME,
     "host": process.env.DB_TEST_HOST,
-    "dialect": process.env.DB_TEST_DIALECT
+    "dialect": process.env.DB_TEST_DIALECT,
+    "use_env_variable": process.env.USE_ENV_VARIABLE
   },
   "stage": {
     "username": process.env.DB_STAGE_USER,
     "password": process.env.DB_STAGE_PASSWORD,
     "database": process.env.DB_STAGE_NAME,
     "host": process.env.DB_STAGE_HOST,
-    "dialect": process.env.DB_STAGE_DIALECT
+    "dialect": process.env.DB_STAGE_DIALECT,
+    "use_env_variable": process.env.USE_ENV_VARIABLE
   },
   "production": {
     "username": process.env.DB_PROD_USER,
     "password": process.env.DB_PROD_PASSWORD,
     "database": process.env.DB_PROD_NAME,
     "host": process.env.DB_PROD_HOST,
-    "dialect": process.env.DB_PROD_DIALECT
+    "dialect": process.env.DB_PROD_DIALECT,
+    "use_env_variable": process.env.USE_ENV_VARIABLE
   }
 }
