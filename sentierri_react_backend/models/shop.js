64c4c66da80632dc9c45d5d4554813dbd9adc3c1
vagasilac <vagasilac@gmail.com>
2023-05-30 12:20:14 +0300

    shop model added

---
+++ sentierri_react_backend/models/shop.js
@@ -0,0 +1,51 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Shop = sequelize.define('Shop', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        telephone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        street_address_1: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        street_address_2: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        zip: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        county: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        country: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Shop',
+        tableName: 'Shops',
+    }
+    );
+
+    return Shop;
+};
