aff8ef037e03db0acc0779fcd7e7c87685595702
vagasilac <vagasilac@gmail.com>
2023-06-12 23:06:28 +0300

    apiSlice is being added for image upload functionality

--- sentierri_react_backend/app.js
+++ sentierri_react_backend/app.js
@@ -40,6 +40,10 @@ app.use('/api/unitsOfMeasure', unitOfMeasureRoutes);
 
 // Add routes for other entities (e.g., products, orders) as needed
 
+// Add your file upload route
+app.use('/api/upload', require('./routes/upload'));
+
+
 // Example route for user login
 const loginRoute = require('./routes/loginRoute');
 app.use('/api/login', passport.authenticate('local', { session: false }), loginRoute);
@@ -48,7 +52,7 @@ app.use('/api/login', passport.authenticate('local', { session: false }), loginR
 const Material = require('./models/material')(sequelize);
 const Category = require('./models/category')(sequelize);
 const Subcategory = require('./models/subcategory')(sequelize);
-const Supplier = require('./models/supplier')(sequeli ze);
+const Supplier = require('./models/supplier')(sequelize);
 const SupplierCategory = require('./models/suppliercategory')(sequelize);
 const SupplierMaterial = require('./models/suppliermaterial')(sequelize);
 const AgentRelation = require('./models/agentrelation')(sequelize);
