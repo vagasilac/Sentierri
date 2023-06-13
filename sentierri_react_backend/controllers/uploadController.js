d4010d67248b9b6e911a27aa09f3bc0bb70ee81a
vagasilac <vagasilac@gmail.com>
2023-06-13 10:50:30 +0300

    UPLOAD misery

--- sentierri_react_backend/controllers/uploadController.js
+++ sentierri_react_backend/controllers/uploadController.js
@@ -2,7 +2,6 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const spacesEndpoint = new AWS.Endpoint('https://sentierri.fra1.cdn.digitaloceanspaces.com'); 
 require('dotenv').config();
 
 const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
@@ -10,7 +9,7 @@ const IAM_USER_KEY = process.env.AWS_ACCESS_KEY;
 const IAM_USER_SECRET = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new AWS.S3({
-  endpoint: spacesEndpoint,
+  endpoint: 'sentierri.fra1.digitaloceanspaces.com',
   accessKeyId: IAM_USER_KEY,
   secretAccessKey: IAM_USER_SECRET,
 });
@@ -21,12 +20,12 @@ const upload = multer({
     bucket: BUCKET_NAME,
     acl: 'public-read',
     metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
+      cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString())
-    }
-  })
+      cb(null, Date.now().toString());
+    },
+  }),
 });
 
 const uploadFile = (req, res) => {
@@ -38,11 +37,11 @@ const uploadFile = (req, res) => {
     Bucket: BUCKET_NAME,
     Key: `${Date.now()}_${req.file.originalname}`, // File name you want to save as in S3
     Body: fileContent,
-    ACL: 'public-read'
+    ACL: 'public-read',
   };
 
   // Uploading files to the bucket
-  s3.upload(params, function(err, data) {
+  s3.upload(params, function (err, data) {
     if (err) {
       throw err;
     }
@@ -53,14 +52,29 @@ const uploadFile = (req, res) => {
       Location: data.Location,
       ETag: data.ETag,
       Key: data.Key,
-      Bucket: data.Bucket
+      Bucket: data.Bucket,
     };
 
-    res.json(responseData);
+    // Use the custom replacer function when calling JSON.stringify
+    const cache = new Set();
+    const replacer = (key, value) => {
+      if (typeof value === 'object' && value !== null) {
+        if (cache.has(value)) {
+          // Circular reference found
+          return;
+        }
+        // Store value in our set
+        cache.add(value);
+      }
+      return value;
+    };
+    
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(responseData, replacer));
   });
 };
 
 module.exports = {
   uploadFile,
-    upload
+  upload,
 };
