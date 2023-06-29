141fa2e8f019903c6b2920d75e962da4156339a0
vagasilac <vagasilac@gmail.com>
2023-06-29 19:42:29 +0300

    modellColor thingy

--- sentierri_react/src/services/modellColorService.js
+++ sentierri_react/src/services/modellColorService.js
@@ -4,7 +4,7 @@ import { API_BASE_URL } from '../config';
 // getModellColor
 export const getModellColor = async (modellId, colorId) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/modellcolors/${modellId}/${colorId}`);
+    const response = await axios.get(`${API_BASE_URL}/modell-colors/${modellId}/${colorId}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching modellColor:', error);
@@ -16,7 +16,7 @@ export const getModellColor = async (modellId, colorId) => {
 export const addModellColor = async (modellId, colorId) => {
   console.log('modellColorService addModellColor','modellId:', modellId, 'colorId:', colorId);
   try {
-    const response = await axios.post(`${API_BASE_URL}/modellcolors/${modellId}/${colorId}`);
+    const response = await axios.post(`${API_BASE_URL}/modell-colors/${modellId}/${colorId}`);
     return response.status === 201; // Return true if the status code is 201 (Created)
   } catch (error) {
     console.error('Error adding modellColor!', error);
@@ -27,7 +27,7 @@ export const addModellColor = async (modellId, colorId) => {
 // getAllModellColors
 export const getAllModellColors = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/modellcolors`);
+    const response = await axios.get(`${API_BASE_URL}/modell-colors/`);
     console.log('response.data:', response);
     return response.data;
   } catch (error) {
@@ -39,7 +39,7 @@ export const getAllModellColors = async () => {
 // removeModellColor
 export const removeModellColor = async (modellId, colorId) => {
   try {
-    const response = await axios.delete(`${API_BASE_URL}/modellcolors/${modellId}/${colorId}`);
+    const response = await axios.delete(`${API_BASE_URL}/modell-colors/${modellId}/${colorId}`);
     if (response.status !== 200) {
       throw new Error('Failed to delete modell color');
     }
@@ -53,8 +53,8 @@ export const removeModellColor = async (modellId, colorId) => {
 // getModellColorsByModellId
 export const getModellColorsByModellId = async (modellId) => {
   try {
-    console.log('getModellColorsByModellId modellId:', modellId, 'API_BASE_URL:', API_BASE_URL, 'API_BASE_URL + /modellcolors/:modellId:', API_BASE_URL + '/modellcolors/:modellId');
-    const response = await axios.get(`${API_BASE_URL}/modellcolors/modell/${modellId}`);
+    console.log('getModellColorsByModellId modellId:', modellId, 'API_BASE_URL:', API_BASE_URL, 'API_BASE_URL + /modell-colors/:modellId:', API_BASE_URL + '/modell-colors/:modellId');
+    const response = await axios.get(`${API_BASE_URL}/modell-colors/modell/${modellId}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching modellColors:', error);
@@ -65,7 +65,7 @@ export const getModellColorsByModellId = async (modellId) => {
 // getModellColorsByColorId
 export const getModellColorsByColorId = async (colorId) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/modellcolors/${colorId}`);
+    const response = await axios.get(`${API_BASE_URL}/modell-colors/${colorId}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching modellColors:', error);
